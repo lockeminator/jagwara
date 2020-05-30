ed523cfe151f80c36861292028695b6ac80d6039
Christian Vietzke <christian.vietzke@gmx.de>
2020-05-30 19:34:44 +0200

    nichts geaendert

--- tpl/header.inc.php
+++ tpl/header.inc.php
@@ -13,7 +13,7 @@ if(isset($_POST['regsenden']))
 
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <title>Kunst.de – ☮</title>
+        <title>Kunst.de – ☮ </title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
         <meta name="keywords" content="Kunstwerke Online kaufen" />
         <meta name="description" content="Erstellen Sie eine Website zu einem frei gewÃ¤hlten Thema." />
